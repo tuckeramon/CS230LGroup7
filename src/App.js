721efa810a2dbc3f9343d7000027b5d0c8523670
tuckeramon <tja00006@mix.wvu.edu>
2024-03-01 11:03:49 -0500

    "Week 8"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
+import Card from './Card.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />;
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
       <p>WVU ID: 800397707</p>
       <p>Hi I am Tucker Amon</p>
+      <Card />;
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,34 @@
+function Card(){
+
+    return(
+        <div class="card-deck">
+        <div class="card">
+            <div class="card-header">
+                <h2 class="card-title">Card 01</h2>
+            </div>
+            <div class="card-body">
+            <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">Card 01</h2>
+        </div>
+            <div class="card-body">
+            <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h2 class="card-title">Card 01</h2>
+            </div>
+            <div class="card-body">
+            <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+    );
+
+}
+
+export default Card;
