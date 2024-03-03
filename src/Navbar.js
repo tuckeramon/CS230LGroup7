0bcecf36659858d422b29b73cfb9f9ba44392235
tuckeramon <tja00006@mix.wvu.edu>
2024-03-02 22:35:40 -0500

    Troubleshooting margins

--- src/Navbar.js
+++ src/Navbar.js
@@ -19,9 +19,9 @@ function Navbar(){
                         <a class="nav-link text-light" href="#">Contact</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <form class="form-inline">
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
         </nav>
