2815fea321ce19e4544f75335cbe7b3beb1a4daf
tuckeramon <tja00006@mix.wvu.edu>
2024-03-02 22:39:31 -0500

    Margin troubleshooting

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar(){
 
     return(
-        <nav class="navbar navbar-expand-lg navbar-darl bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-darl bg-dark col-sm-12">
             <a class="navbar-brand text-light" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
