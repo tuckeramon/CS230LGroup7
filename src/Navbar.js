d9a0a72d28534589d065da2d7e3b4d12a5756677
tuckeramon <tja00006@mix.wvu.edu>
2024-03-02 21:34:16 -0500

    Troubleshooting Navbar 3

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,16 +7,16 @@ function Navbar(){
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse text-light" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link text-light" href="#">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link text-light" href="#">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a class="nav-link text-light" href="#">Contact</a>
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
