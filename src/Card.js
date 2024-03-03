380d1edc85672dc4599c28c1fa3d011502c903a2
tuckeramon <tja00006@mix.wvu.edu>
2024-03-02 20:50:12 -0500

    Attempt 2 to get all cards on one row

--- src/Card.js
+++ src/Card.js
@@ -2,30 +2,36 @@ function Card(){
 
     return(
         <div class="row">
-        <div class="card text-white bg-primary">
-            <div class="card-header">
-                <h2 class="card-title">Card 01</h2>
+            <div class="col-sm-4">
+                <div class="card text-white bg-primary">
+                    <div class="card-header">
+                        <h2 class="card-title">Card 01</h2>
+                    </div>
+                    <div class="card-body">
+                    <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
             </div>
-            <div class="card-body">
-            <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="col-sm-4">
+                <div class="card text-white bg-success">
+                <div class="card-header">
+                    <h2 class="card-title">Card 01</h2>
+                </div>
+                    <div class="card-body">
+                    <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="card text-white bg-success">
-        <div class="card-header">
-            <h2 class="card-title">Card 01</h2>
-        </div>
-            <div class="card-body">
-            <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="col-sm-4">
+            <div class="card text-white bg-danger">
+                <div class="card-header">
+                    <h2 class="card-title">Card 01</h2>
+                </div>
+                <div class="card-body">
+                <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
             </div>
-        </div>
-        <div class="card text-white bg-danger">
-            <div class="card-header">
-                <h2 class="card-title">Card 01</h2>
             </div>
-            <div class="card-body">
-            <p class="card-text">Some example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
         </div>
     );
 
