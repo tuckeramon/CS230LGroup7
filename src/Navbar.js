0dc4645ec4a4268435335679b5af7507678b0cb2
tuckeramon <tja00006@mix.wvu.edu>
2024-03-02 22:48:42 -0500

    More navbar troubleshooting

--- src/Navbar.js
+++ src/Navbar.js
@@ -20,8 +20,8 @@ function Navbar(){
                     </li>
                 </ul>
                 <form class="form-inline">
-                    <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
         </nav>
